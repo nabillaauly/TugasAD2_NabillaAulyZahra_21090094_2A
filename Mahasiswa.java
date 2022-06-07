826f04bed1f84f3e1ac1175c1078afc2b8b83ec4
nabillaauly <101704002+nabillaauly@users.noreply.github.com>
2022-06-07 21:50:29 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,24 @@
+package Mahasiswa;
+
+public class Mahasiswa {
+    private String Shinta;
+    private String Jefri;
+    private String Olif;
+    private String Surya;
+    private String Dani;
+    private String Winda;
+
+    public Mahasiswa(String shinta,String jefri,String olif,String surya,String dani,String winda){
+        this.Shinta = Shinta;
+        this.Jefri = Jefri;
+        this.Olif = Olif;
+        this.Surya = Surya;
+        this.Dani = Dani;
+        this.Winda = Winda;
+    }
+    public class PrintMahasiswa{
+        public void PrintMahasiswa(){
+            System.out.println("Nama Mahasiswa ;");
+        }
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Masukkan Nama Mahasiswa :");
+        int nm_mhswa = scan.nextInt();
+        int[] data = new int[nm_mhswa];
+        System.out.println("Shinta,Jefri,Olif,Surya,Dani,Winda");
+    }
+}
